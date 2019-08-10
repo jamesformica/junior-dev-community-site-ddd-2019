46d8e72fcb22b68e4092c7a73298e9d62b48fdc9
James Formica <james.formica@gmail.com>
2019-08-10 12:43:46 +1000

    DJ IS A DONUT

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function() {
   console.log('💜 all systems are go!')
 
+  window.setInterval(() => this.document.getElementById("parrot").append("Hello"), 2000)
   // MOAR JAVASCRIPT
 })
