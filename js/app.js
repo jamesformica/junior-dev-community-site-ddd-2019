64e66532c9f26ebd30767c97d7df6cdac9bec509
James Formica <james.formica@gmail.com>
2019-08-10 13:35:51 +1000

    cheeky clicky footer boi

--- js/app.js
+++ js/app.js
@@ -2,5 +2,8 @@ window.addEventListener('load', function() {
   console.log('💜 all systems are go!')
 
   window.setInterval(() => this.document.getElementById("parrot").append("Hello"), 2000)
+  this.document.querySelector(".jd_footer").addEventListener("click", function(){
+    window.alert("Thanks for clicking, you're awesome!");
+  })
   // MOAR JAVASCRIPT
 })
